c8f3be3728c4fe7569b6733c3d312aed84809b9b
Zfahruk <synergyetkisi@gmail.com>
2020-06-21 21:33:46 +0200

    feature branch'ten ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,10 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Ilk commit from repository");
 		System.out.println("ikinci commit from repository");
+		
+		System.out.println("Yeni branch olusturdum adi feature");
+		
+		
 
 	}
 
