34fd60877410a7f2672107ab569bee291393739f
Zfahruk <synergyetkisi@gmail.com>
2020-06-21 21:14:48 +0200

    ikinci commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("Ilk commit from repository");
+		System.out.println("ikinci commit from repository");
 
 	}
 
